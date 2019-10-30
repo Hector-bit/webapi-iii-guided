264b241000bf42997d9bc764d3a2407abaa8d6c9
Hector-bit <hectormartinez.2978@gmail.com>
2019-10-30 10:34:27 -0700

    no password displays prompt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
 const hubsRouter = require('./hubs/hubs-router.js');
-
+const morgan = require('morgan');
 const server = express();
 
 function dateLogger(req, res, next) {
@@ -11,12 +11,29 @@ function dateLogger(req, res, next) {
 }
 
 function httpLogger(req, res, next){
-  console.log(res.status(200).json(req))
-}
+  console.log(
+    `The Logger: [${new Date().toISOString}].${req.method} t0 ${req.url}`
+  )}
+
+  function gateKeeper(req, res, next) {
+    // data can come in the body, url parameters, query string, headers
+    // new way of reading data sent by the client
+    const password = req.headers.password || '';
+    if (password.toLowerCase() === '') {
+      res.status(400).json('please enter a password')
+    }
+    if (password.toLowerCase() === 'mellon') {
+      next();
+    } else {
+      res.status(400).json({ you: 'cannot pass!!' });
+    }
+  }
+  
 
 //global middleware
-server.use(helmet())
-
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(gateKeeper);
 server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
